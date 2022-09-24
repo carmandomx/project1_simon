c765e45e0bd100bb2483563c2f1b8c6a4e68fe57
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-09-24 01:12:18 -0500

    feat: Add start button

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 const square = document.querySelector('.square');
 const squareContainer = document.querySelector('.square-container');
+const btnStart = document.querySelector('#btn-start');
+const colors = ['red', 'green', 'yellow', 'blue'];
 // function for button animation
 const squareAnimation = (color) => {
   // Variable for select button using the color clicked
   const square = document.querySelector(`.square-${color}`);
-  // Add styles to the square clicked
+  // Add styles to the color button clicked
   square.classList.add('pushed');
   // Remove styles after 200 milliseconds for push effect
   setTimeout(() => {
     square.classList.remove('pushed');
   }, 200);
 };
-// function to show the animation when the color is picked
+
+const circleAnimation = () => {
+  const circleStart = document.querySelector('.circle');
+  console.log(circleStart);
+  circleStart.classList.add('pushed');
+  setTimeout(() => {
+    circleStart.classList.remove('pushed');
+  }, 200);
+};
+
+// Function to show the animation when the color is clicked
 // it calls the function "squareAnimation"
 const playerRound = () => {
+  // Add event to the container to get the color of clicked button
   squareContainer.addEventListener('click', (event) => {
+    // Condition to confirm that a square is clicked
     if (event.target.classList.contains('square')) {
       const color = event.target.id;
+      // calls animation for the button and passes the color
       squareAnimation(color);
     }
   });
 };
-// Starts the listening of click events
-playerRound();
+// Function to start the game
+const start = () => {
+  // When we are in a game the button will be disabled
+  btnStart.setAttribute('disabled', true);
+  playerRound();
+};
+// Eventlistener for the start button
+btnStart.addEventListener('click', () => {
+  // Calls the animation for button start
+  circleAnimation();
+  // Starts the game
+  start();
+});
