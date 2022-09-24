a4a179296e4d85b29e2d375973b3b7e7ef625716
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-09-24 00:32:01 -0500

    Add comments

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const square = document.querySelector('.square');
 const squareContainer = document.querySelector('.square-container');
+// function for button animation
 const squareAnimation = (color) => {
+  // Variable for select button using the color clicked
   const square = document.querySelector(`.square-${color}`);
-  // square.setAttribute('class', '.pushed');
+  // Add styles to the square clicked
   square.classList.add('pushed');
+  // Remove styles after 200 milliseconds for push effect
   setTimeout(() => {
     square.classList.remove('pushed');
   }, 200);
 };
-
+// function to show the animation when the color is picked
+// it calls the function "squareAnimation"
 const playerRound = () => {
   squareContainer.addEventListener('click', (event) => {
     if (event.target.classList.contains('square')) {
@@ -16,8 +20,6 @@ const playerRound = () => {
       squareAnimation(color);
     }
   });
-
-  // return sequenceOK;
 };
-
+// Starts the listening of click events
 playerRound();
