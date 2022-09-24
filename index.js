2a6b96fb0c427fc3addc75dc1ee50f7990c35ebc
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-09-24 00:19:58 -0500

    feat: Add game interface with clickable buttons

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Live reloading')
+const square = document.querySelector('.square');
+const squareContainer = document.querySelector('.square-container');
+const squareAnimation = (color) => {
+  const square = document.querySelector(`.square-${color}`);
+  // square.setAttribute('class', '.pushed');
+  square.classList.add('pushed');
+  setTimeout(() => {
+    square.classList.remove('pushed');
+  }, 200);
+};
+
+const playerRound = () => {
+  squareContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('square')) {
+      const color = event.target.id;
+      squareAnimation(color);
+    }
+  });
+
+  // return sequenceOK;
+};
+
+playerRound();
