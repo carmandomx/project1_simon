17e9ba9a0420931af35cb165fa688cd6fb1892c1
FranzRivas-Ksquare <franz.rivas@itksquare.edu.mx>
2022-09-24 21:43:05 -0500

    just replace binary to links sounds

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const audioGreen = new Audio("./media/sounds/button-green-true.mp3");
+const audioGreen = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
 audioGreen.autoplay = true;
 
-const audioRed = new Audio("./media/sounds/button-red-true.mp3");
+const audioRed = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
 audioRed.autoplay = true;
 
-const audioYellow = new Audio ("./media/sounds/button-yellow-true.mp3");
+const audioYellow = new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
 audioYellow.autoplay = true;
 
-const audioBlue = new Audio ("./media/sounds/button-blue-true.mp3");
+const audioBlue = new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 audioBlue.autoplay = true;
 
 const audioStart = new Audio ("./media/sounds/button-start.mp3");
